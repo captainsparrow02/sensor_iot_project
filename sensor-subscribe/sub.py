1611bc4d0ebde809ea070d37e1cf69381febd0be
captainsparrow02 <tabishnaved50@gmail.com>
2023-08-28 22:59:38 +0530

    Added Redis

--- sensor-subscribe/sub.py
+++ sensor-subscribe/sub.py
@@ -2,6 +2,7 @@ import paho.mqtt.client as mqtt
 import sys
 import pymongo
 import json
+import redis
 # import redis
 
 # MongoDB
@@ -10,13 +11,19 @@ db = mongo_client['sensors']
 collections_temp = db['temp']
 collections_humid = db['humid']
 
+# Redis 
+# Redis
+redis_host = "redis"
+redis_port = 6379
+redis_client = redis.Redis(host=redis_host, port=redis_port)
+
 # MQTT 
 client = mqtt.Client("Subscriber")
 mqttBroker = "mqtt"
 port = 1883
 topic_humidity = "sensor/reading/humidity"
 topic_temperature = "sensor/reading/temperature"
-# client.username_pw_set("user1","1234")
+
 qos = 0
 
 def on_log(client, userdata, level, buf):
@@ -40,23 +47,31 @@ def on_message_for_temperature(client, userdata, message):
 
     print("temp", payload)
     try:
+        redis_client.lpush("temperature_readings", json.dumps(payload))
+        redis_client.ltrim("temperature_readings", 0, 9)
+    except Exception as e:
+        print("Redis insert error")
+        print(e)
+    try:
         collections_temp.insert_one(payload)
     except Exception as e:
         print("Data could not be inserted.")
         print(e)
-
-    # redis_client.lpush("sensor_readings", json.dumps(payload))
-    # redis_client.ltrim("sensor_readings", 0, 9)
-    # collections.insert_one(payload)
 
 def on_message_for_humidity(client, userdata, message):
     print("Recieved Message: ", str(message.payload.decode('utf-8')))
     payload = json.loads(message.payload.decode('utf-8'))
     print("Humid", payload)
     try:
+        redis_client.lpush("humidity_readings", json.dumps(payload))
+        redis_client.ltrim("humidity_readings", 0, 9)
+    except:
+        print("Redis insert error")
+    try:
         collections_humid.insert_one(payload)
     except:
         print("Data could not be inserted.")
+    
 
 # Attempting Connection
 client.on_connect = on_connect
